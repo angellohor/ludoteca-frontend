167860195494a4143cde619a499f45beca88af38
angellohor <Angel.Lopez30@alu.uclm.es>
2026-04-28 13:14:13 +0200

    Initial commit: project setup and category CRUD

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { path:"categories", loadComponent:() => import('./category/category-list/category-list').then(m => m.CategoryList)}
+];
---
+++ src/app/author/author-list/author-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-list',
+  imports: [],
+  templateUrl: './author-list.html',
+  styleUrl: './author-list.scss',
+})
+export class AuthorList {}
